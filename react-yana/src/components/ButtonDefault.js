7edcfaa3a714dfff70e1c780de59852fff5b8f8a
teck7 <teckyap@hotmail.com.au>
2017-07-03 19:16:15 +1000

    Style buttonDefault

--- react-yana/src/components/ButtonDefault.js
+++ react-yana/src/components/ButtonDefault.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 const ButtonDefault = ({title, buttonPath}) => (
-  <div className="button-container text-center">
-     <button className="btn learn-now-button">
-       <Link to={buttonPath}>{title}</Link>
+     <button className="btn button-Default">
+       <Link className="button-default-link" to={buttonPath}>{title}</Link>
      </button>
- </div>
 )
 
 export default ButtonDefault;
--- react-yana/src/pages/HomePage.js
+++ react-yana/src/pages/HomePage.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Image } from 'react-bootstrap';
 import Footer from '../components/Footer';
 import ButtonDefault from '../components/ButtonDefault';
-import vaclLogo from '../img/vacl_logo.jpg';
-import boomerang from '../img/boomerang.jpg';
+import vaclLogo from '../img/logo.png';
+import boomerang from '../img/boomerang.png';
 import vaclLogoMod from '../img/vacl_b_logo.jpg';
 import bio from '../img/bio.jpg';
 
@@ -18,8 +18,10 @@ const HomePage = ({
             <Image className="vacl-logo" src={vaclLogo} alt="vacl_logo_caption" responsive/>
             <h1 className="home-heading-text text-center">Dive into Aboriginal Culture</h1>
             <Image className="boom-img" src={boomerang} alt="boomerang_caption" responsive/>
-            <ButtonDefault title="Learn Now"
-                    buttonPath="/courses"  />
+            <div className="learn-now-button-container text-center">
+                <ButtonDefault title="Learn Now"
+                               buttonPath="/courses"  />
+            </div>
             <h6 className="h6-text text-center">Not a new user?</h6>
             <div className="sign-in-link-container text-center">
               <a className="sign-in-link" href="#">Sign in</a>
