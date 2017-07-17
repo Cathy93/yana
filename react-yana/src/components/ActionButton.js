8cab984b5bb6063c459c804549c3e211c5798d81
teck7 <teckyap@hotmail.com.au>
2017-07-17 11:12:14 +1000

    change css on action button

--- react-yana/src/components/ActionButton.js
+++ react-yana/src/components/ActionButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ActionButton = ({title, onClick}) => (
   <div className="button-container text-center">
-     <button className="btn button-thin" onClick={onClick}>
+     <button className="btn action-btn" onClick={onClick}>
        {title}
      </button>
  </div>
