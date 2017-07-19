d8ab741d95651a8568a1593e73f9748ea871c005
teck7 <teckyap@hotmail.com.au>
2017-07-19 14:51:35 +1000

    adjust css for signin and signout

--- react-yana/src/pages/SignInPage.js
+++ react-yana/src/pages/SignInPage.js
@@ -2,18 +2,27 @@ import React from 'react'
 import { Redirect } from 'react-router-dom'
 import SignInForm from '../components/SignInForm'
 import LanguagesPage from './LanguagesPage'
+import Footer from '../components/Footer';
+
 const SignInPage = ({
     token,
     onSignIn
 }) => (
     <div>
-    {
-        !!token ? (
-            <LanguagesPage token={ token} />
-        ) : (
-            <SignInForm onSignIn={ onSignIn } />
-        )
-    }
+      <h1 className="home-heading-text text-center">Welcome Back!</h1>
+      <div>
+      {
+          !!token ? (
+              <LanguagesPage token={ token} />
+          ) : (
+
+              <SignInForm onSignIn={ onSignIn } />
+          )
+      }
+      </div>
+      <div className="fixed-bottom">
+        <Footer />
+      </div>
     </div>
 )
 
--- react-yana/src/pages/SignUpPage.js
+++ react-yana/src/pages/SignUpPage.js
@@ -2,19 +2,26 @@ import React from 'react'
 import { Redirect } from 'react-router-dom'
 import SignUpForm from '../components/SignUpForm'
 import LanguagesPage from './LanguagesPage'
+import Footer from '../components/Footer';
 
 const SignUpPage = ({
     token,
     onSignUp
 }) => (
     <div>
-    {
-        !!token ? (
-            <LanguagesPage token={ token } />
-        ) : (
-            <SignUpForm onSignUp={ onSignUp } />
-        )
-    }
+      <h1 className="home-heading-text text-center">Join Us</h1>
+      <div>
+      {
+          !!token ? (
+              <LanguagesPage token={ token } />
+          ) : (
+              <SignUpForm onSignUp={ onSignUp } />
+          )
+      }
+      </div>
+      <div className="fixed-bottom">
+        <Footer />
+      </div>
     </div>
 )
 
