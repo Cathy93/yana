0a94dbfbbfe4f966c538cd883bf23e6ab003c5c8
Cathy93 <CathyMarion93@gmail.com>
2017-07-24 13:18:41 +1000

    cors

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const authMiddleware = require('../middleware/auth')
-
+const cors = require('cors')
 const router = express.Router()
 
-router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors())
 // Sign in: POST /auth
 router.post('/auth', authMiddleware.authenticateSignIn, authMiddleware.signTokenHandler)
 
