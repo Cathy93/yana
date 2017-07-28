0b52e44d24dec09abb7c937a4dbc5b7b13262ebc
Livia_Santos <liviamelq@gmail.com>
2017-07-28 14:47:40 +1000

    Fix sinUp form

--- react-yana/src/components/SignUpForm.js
+++ react-yana/src/components/SignUpForm.js
@@ -22,40 +22,25 @@ export default function SignUpForm({
   onSignUp
 }) {
   return (
-        <div>
-          <form className="form-container" onSubmit={ (event) => submitSignUp(event, onSignUp) }>
-
-            <div className="label-container col-xs-12 col-sm-12 col-md-4 col-lg-4">
-              <label className="label-text-user">Email:  &nbsp;</label>
-            </div>
-            <div className="input-container col-xs-12 col-sm-12 col-md-8 col-lg-8">
-              <input className="form-control-user" type='email' name='email' />
-            </div>
-
-            <div className="label-container col-xs-12 col-sm-12 col-md-4 col-lg-4">
-              <label className="label-text-user">Password:  &nbsp;</label>
-            </div>
-            <div className="input-container col-xs-12 col-sm-12 col-md-8 col-lg-8">
-              <input className="form-control-user" type='password' name='password' />
+        <div className='container'>
+          <form onSubmit={ (event) => submitSignUp(event, onSignUp) }>
+          <div className="form-group">
+            <label className="label-text-user">Email:</label>
+            <input className="form-control" type='email' name='email' />
+            <label className="label-text-user">Password:</label>
+            <input className="form-control" type='password' name='password' />
             </div>
 
-            <div className="col-lg-12">
-              <div className="learn-now-button-container text-center">
-                <button className="btn thin-btn">Join</button>
-              </div>
+            <div className="learn-now-button-container text-center">
+              <button className="btn thin-btn">Join</button>
             </div>
           </form>
 
           <div className="text-center user-link-margin">
             <h3 className="">Already an existing User? </h3>
-            <div className="sign-in-link-container text-center">
-            <AnchorLink title="Sign In" linkPath="/sign_in" />
-            </div>
+            <AnchorLink className="sign-in-link-container" title="Sign In" linkPath="/sign_in" />
           </div>
-
-          <div>
-            <Footer />
-          </div>
+          <Footer />
 
       </div>
     )
